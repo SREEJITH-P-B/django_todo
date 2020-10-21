8e46dad98449ec3b9330b3b04ec1c48aa8011cab
SREEJITH-P-B <33229709+SREEJITH-P-B@users.noreply.github.com>
2020-10-21 14:13:40 +0530

    Update settings.py

--- django_basics/settings.py
+++ django_basics/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forms_django_learn',
+    'todoapp',
 ]
 
 MIDDLEWARE = [
